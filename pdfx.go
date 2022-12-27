2c0793cf3572ad52336026e6cce6f23f3cc4bf09
Raiyan Jamil <73040210+Raiyanx@users.noreply.github.com>
2022-12-26 23:53:58 -0500

    added command line args

--- pdfx.go
+++ pdfx.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/raiyanx/pdfx/merge"
+	"github.com/raiyanx/pdfx/split"
 )
 
+func printUsage() {
+	usage := `Usage:
+	pdfx merge [files]
+	pdfx split filename [Integer/Integer-Integer]	
+`
+	fmt.Println(usage)
+}
+
 func main() {
-	merge.MergePrint()
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		printUsage()
+	} else if args[0] == "merge" {
+		merge.MergePrint()
+	} else if args[0] == "split" {
+		split.SplitPrint()
+	} else {
+		printUsage()
+	}
 }
---
+++ split/split.go
@@ -0,0 +1,9 @@
+package split
+
+import (
+	"fmt"
+)
+
+func SplitPrint() {
+	fmt.Println("I used Split")
+}
